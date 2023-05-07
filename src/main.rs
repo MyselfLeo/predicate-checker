43e4b7a8d838bfbab9647c1749f5959d26a2c087
MyselfLeo <myselfleo@outlook.fr>
2023-05-07 18:08:20 +0200

    not the best but not the worse

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use implicity::parser;
 
 fn main() {
-    let p1 = parser::parse_predicate("(x >= 3) && (x <= 3)");
-    println!("Predicate: {:?}", p1);
-    println!("Domain: {:?}", p1.get_domain());
+    let p1 = parser::parse_predicate("((x >= 3) && (x <= 3)) || (y == 3)");
+    let p2 = parser::parse_predicate("(y == 2) || (x == 3)");
+    println!("{}", p1.fits(&p2));
 }
--- src/predicate.rs
+++ src/predicate.rs
@@ -1,4 +1,4 @@
-use std::fmt::{Display, Debug};
+use std::{fmt::{Display, Debug}, collections::HashSet};
 
 use num::{Num, ToPrimitive};
 
@@ -17,11 +17,6 @@ pub enum Value<T: Num + PartialOrd> {
 
 
 /// A predicate is a boolean expression that can contain arguments with unknown values.
-/// They can be simplified without evaluation.
-/// 
-/// It is possible to check if a given Predicate implies another Predicate, i.e. if for every
-/// values where A is true, B is true for those values.
-/// This is done without evaluating the Predicate.
 #[derive(Debug, Clone, PartialEq)]
 pub enum Predicate<T: Num + PartialOrd> {
     True,
@@ -46,20 +41,35 @@ pub enum Predicate<T: Num + PartialOrd> {
 
 impl<T: Num + PartialOrd + Clone + ToPrimitive + Display + Debug> Predicate<T> {
 
-    /// Return the domain representing the values for which the predicate is true.
-    /// If the predicate is based solely on arguments, the domain is unknown, so a full domain is returned.
-    pub fn get_domain(&self, arg_name: String) -> Domain<T> {
+    /// Return the domain representing the values of the given argument where the Predicate is true.
+    /// Thus, the "validity domain" of a predicate is made of one domain for each of its arguments.
+    /// A Predicate with no arguments could be simplified to a single boolean value, so this function would not be useful.
+    pub fn get_domain(&self, arg_name: &str) -> Domain<T> {
 
         match self {
             Predicate::True => Domain::_true(),
             Predicate::False => Domain::_false(),
-            Predicate::BoolArg(_) => Domain::_true(), // Unknown value => full domain
+
+            // if _ is arg_name: unknown value so the validity domain could be anything
+            // if _ is not arg_name: the value of arg_name is irrelevant so the domain of validity is any values
+            Predicate::BoolArg(_) => Domain::_true(),
 
             Predicate::LowerThan(v1, v2) => {
                 match (v1, v2) {
-                    (Value::Arg(_), Value::Arg(_)) => Domain::_true(),
-                    (Value::Arg(_), Value::Literal(x)) => Domain::new(None, false, Some(x.clone()), false),
-                    (Value::Literal(x), Value::Arg(_)) => Domain::new(Some(x.clone()), false, None, false),
+                    (Value::Arg(a1), Value::Arg(a2)) => {
+                        if a1 == a2 {Domain::_false()} else {Domain::_true()} // the same argument cannot be lower than itself, so the Predicate is always false
+                    },
+
+                    (Value::Arg(a), Value::Literal(x)) => {
+                        if a == arg_name {Domain::new(None, false, Some(x.clone()), false)}
+                        else {Domain::_true()}
+                    },
+
+                    (Value::Literal(x), Value::Arg(a)) => {
+                        if a == arg_name {Domain::new(Some(x.clone()), false, None, false)}
+                        else {Domain::_true()}
+                    },
+
                     (Value::Literal(x1), Value::Literal(x2)) => if x1 < x2 {Domain::_true()} else {Domain::_false()},
                 }
             },
@@ -68,8 +78,15 @@ impl<T: Num + PartialOrd + Clone + ToPrimitive + Display + Debug> Predicate<T> {
             Predicate::LowerEqual(v1, v2) => {
                 match (v1, v2) {
                     (Value::Arg(_), Value::Arg(_)) => Domain::_true(),
-                    (Value::Arg(_), Value::Literal(x)) => Domain::new(None, false, Some(x.clone()), true),
-                    (Value::Literal(x), Value::Arg(_)) => Domain::new(Some(x.clone()), true, Some(x.clone()), false),
+
+                    (Value::Arg(a), Value::Literal(x)) => {
+                        if a == arg_name {Domain::new(None, false, Some(x.clone()), true)}
+                        else {Domain::_true()}
+                    },
+                    (Value::Literal(x), Value::Arg(a)) => {
+                        if a == arg_name {Domain::new(Some(x.clone()), true, Some(x.clone()), false)}
+                        else {Domain::_true()}
+                    },
                     (Value::Literal(x1), Value::Literal(x2)) => if x1 <= x2 {Domain::_true()} else {Domain::_false()},
                 }
             },
@@ -77,9 +94,18 @@ impl<T: Num + PartialOrd + Clone + ToPrimitive + Display + Debug> Predicate<T> {
 
             Predicate::GreaterThan(v1, v2) => {
                 match (v1, v2) {
-                    (Value::Arg(_), Value::Arg(_)) => Domain::_true(),
-                    (Value::Arg(_), Value::Literal(x)) => Domain::new(Some(x.clone()), false, None, false),
-                    (Value::Literal(x), Value::Arg(_)) => Domain::new(None, false, Some(x.clone()), false),
+                    (Value::Arg(a1), Value::Arg(a2)) => {
+                        if a1 == a2 {Domain::_false()} else {Domain::_true()} // the same argument cannot be greater than itself, so the Predicate is always false
+                    },
+
+                    (Value::Arg(a), Value::Literal(x)) => {
+                        if a == arg_name {Domain::new(Some(x.clone()), false, None, false)}
+                        else {Domain::_true()}
+                    },
+                    (Value::Literal(x), Value::Arg(a)) => {
+                        if a == arg_name {Domain::new(None, false, Some(x.clone()), false)}
+                        else {Domain::_true()}
+                    },
                     (Value::Literal(x1), Value::Literal(x2)) => if x1 > x2 {Domain::_true()} else {Domain::_false()},
                 }
             },
@@ -88,8 +114,14 @@ impl<T: Num + PartialOrd + Clone + ToPrimitive + Display + Debug> Predicate<T> {
             Predicate::GreaterEqual(v1, v2) => {
                 match (v1, v2) {
                     (Value::Arg(_), Value::Arg(_)) => Domain::_true(),
-                    (Value::Arg(_), Value::Literal(x)) => Domain::new(Some(x.clone()), true, None, false),
-                    (Value::Literal(x), Value::Arg(_)) => Domain::new(None, false, Some(x.clone()), true),
+                    (Value::Arg(a), Value::Literal(x)) => {
+                        if a == arg_name {Domain::new(Some(x.clone()), true, None, false)}
+                        else {Domain::_true()}
+                    },
+                    (Value::Literal(x), Value::Arg(a)) => {
+                        if a == arg_name {Domain::new(None, false, Some(x.clone()), true)}
+                        else {Domain::_true()}
+                    },
                     (Value::Literal(x1), Value::Literal(x2)) => if x1 >= x2 {Domain::_true()} else {Domain::_false()},
                 }
             },
@@ -98,26 +130,133 @@ impl<T: Num + PartialOrd + Clone + ToPrimitive + Display + Debug> Predicate<T> {
             Predicate::Equal(v1, v2) => {
                 match (v1, v2) {
                     (Value::Arg(_), Value::Arg(_)) => Domain::_true(),
-                    (Value::Arg(_), Value::Literal(x)) => Domain::point(x.clone()),
-                    (Value::Literal(x), Value::Arg(_)) =>  Domain::point(x.clone()),
+                    (Value::Arg(a), Value::Literal(x)) => {
+                        if a == arg_name {Domain::point(x.clone())}
+                        else {Domain::_true()}
+                    },
+                    (Value::Literal(x), Value::Arg(a)) => {
+                        if a == arg_name {Domain::point(x.clone())}
+                        else {Domain::_true()}
+                    },
                     (Value::Literal(x1), Value::Literal(x2)) => if x1 == x2 {Domain::_true()} else {Domain::_false()},
                 }
             },
 
-            Predicate::Not(p) => Domain::complement(p.get_domain()),
-            Predicate::And(p1, p2) => Domain::intersection(p1.get_domain(), p2.get_domain()),
-            Predicate::Or(p1, p2) => Domain::union(p1.get_domain(), p2.get_domain()),
+            Predicate::Not(p) => Domain::complement(p.get_domain(arg_name)),
+            Predicate::And(p1, p2) => Domain::intersection(p1.get_domain(arg_name), p2.get_domain(arg_name)),
+            Predicate::Or(p1, p2) => Domain::union(p1.get_domain(arg_name), p2.get_domain(arg_name)),
         }
     }
 
 
-    /// Check that each value that satisfies the first Predicate also satisfies the second Predicate.
-    /// In other terms, check if the Domain of the first Predicate is a subset of the Domain of the second Predicate.
-    pub fn fits(&self, other: &Predicate<T>) -> bool {
-        let d1 = self.get_domain().simplified();
-        let d2 = other.get_domain().simplified();
-        
-        return Domain::union(d1, d2.clone()) == d2;
+
+
+
+    /// Return the set of arguments used by the predicate
+    pub fn get_arguments(&self) -> HashSet<String> {
+        match self {
+            Predicate::True => HashSet::new(),
+            Predicate::False => HashSet::new(),
+            Predicate::BoolArg(a) => {
+                let mut set = HashSet::new();
+                set.insert(a.clone());
+                set
+            },
+            Predicate::LowerThan(v1, v2) => {
+                let mut set = HashSet::new();
+                match v1 {
+                    Value::Arg(a) => set.insert(a.clone()),
+                    Value::Literal(_) => false,
+                };
+                match v2 {
+                    Value::Arg(a) => set.insert(a.clone()),
+                    Value::Literal(_) => false,
+                };
+                set
+            },
+            Predicate::LowerEqual(v1, v2) => {
+                let mut set = HashSet::new();
+                match v1 {
+                    Value::Arg(a) => set.insert(a.clone()),
+                    Value::Literal(_) => false,
+                };
+                match v2 {
+                    Value::Arg(a) => set.insert(a.clone()),
+                    Value::Literal(_) => false,
+                };
+                set
+            },
+            Predicate::GreaterThan(v1, v2) => {
+                let mut set = HashSet::new();
+                match v1 {
+                    Value::Arg(a) => set.insert(a.clone()),
+                    Value::Literal(_) => false,
+                };
+                match v2 {
+                    Value::Arg(a) => set.insert(a.clone()),
+                    Value::Literal(_) => false,
+                };
+                set
+            },
+            Predicate::GreaterEqual(v1, v2) => {
+                let mut set = HashSet::new();
+                match v1 {
+                    Value::Arg(a) => set.insert(a.clone()),
+                    Value::Literal(_) => false,
+                };
+                match v2 {
+                    Value::Arg(a) => set.insert(a.clone()),
+                    Value::Literal(_) => false,
+                };
+                set
+            },
+            Predicate::Equal(v1, v2) => {
+                let mut set = HashSet::new();
+                match v1 {
+                    Value::Arg(a) => set.insert(a.clone()),
+                    Value::Literal(_) => false,
+                };
+                match v2 {
+                    Value::Arg(a) => set.insert(a.clone()),
+                    Value::Literal(_) => false,
+                };
+                set
+            },
+            Predicate::Not(p) => p.get_arguments(),
+            Predicate::And(p1, p2) => {
+                let mut set = p1.get_arguments();
+                for a in p2.get_arguments() {set.insert(a);}
+                set
+            },
+            Predicate::Or(p1, p2) => {
+                let mut set = p1.get_arguments();
+                for a in p2.get_arguments() {set.insert(a);}
+                set
+            },
+        }
     }
 
+
+
+
+
+
+    /// A Predicate A "fits" a Predicate B if:
+    /// - A and B use the same arguments
+    /// - For every argument used by both A and B, the validity domain of A is a subset of the validity domain of B
+    pub fn fits(&self, other: &Predicate<T>) -> bool {
+        let self_args = self.get_arguments();
+        let other_args = other.get_arguments();
+
+        if self_args != other_args {return false;}
+
+        // Check validity domains for arguments used both by A and B
+        for a in self_args.iter() {
+            let d1 = self.get_domain(a);
+            let d2 = other.get_domain(a);
+            if Domain::union(d1, d2.clone()) != d2 {return false;}
+        }
+
+        true
+    }
 }
