10d7f071debee42651ed46c81c81e3eee26c6283
MyselfLeo <myselfleo@outlook.fr>
2023-05-07 18:22:13 +0200

    changed crate name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use implicity::parser;
+use predicatechecker::parser;
 
 fn main() {
-    let a: implicity::Predicate<f64> = parser::parse_predicate("(x > 2) && (y == 4) && (z < 10)");
+    let a: predicatechecker::Predicate<f64> = parser::parse_predicate("(x > 2) && (y == 4) && (z < 10)");
     let b = parser::parse_predicate("(x > 0) && (y > 2)");
 
     println!("{}", a.fits(&b));
