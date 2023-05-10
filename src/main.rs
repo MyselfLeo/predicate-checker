53a962df0e62c59cd5c2aa0c39eb84076416c393
MyselfLeo <myselfleo@outlook.fr>
2023-05-10 20:11:34 +0200

    updated readme + version nb

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
 use predicatechecker::Predicate;
 
 fn main() {
-    //let a = Predicate::from("x > 2 && y == 4").unwrap();
-    //let b = Predicate::from("x > 3 && y > 2").unwrap();
-
-    let a = Predicate::from("x > 0").unwrap();
-    let b = Predicate::from("x > -3").unwrap();
+    let a = Predicate::from("(x > 2) && (y == 4) && (z < 10)").unwrap();
+    let b = Predicate::from("(x > 0) && (y > 2)").unwrap();
 
     println!("{:?}", a.implies(&b));
-
-    //let b = Predicate::from("(x > 0) && (y > 2)").unwrap();
-    //println!("{}", a.implies(&b));
 }
